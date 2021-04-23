080c5ba9412672bf66612ff14fc83caa0b0185f8
AsselS-campus <assel.sultangazieva@le-campus-numerique.fr>
2021-04-23 11:40:45 +0200

    without comments

--- MotMystere.cpp
+++ MotMystere.cpp
@@ -11,20 +11,22 @@ using namespace std;
 int main()
 {
    
-    /*string motMystere("Procrastination");
+    
     //1 : On demande de saisir un mot
+
+    string motMystere("Procra");
     cout << "Entrer un mot : ";
     std::cin >> motMystere;
     cout << "Le mot est :" << motMystere;
-    return 0;*/
+    return 0;
 
     //3 : On demande à l'utilisateur quel est le mot mystère
-    string motMystere("Procra");
+  
     cout << "Saisissez un mot : " << endl;;
     cin >> motMystere;
     cout << "Le mot est :" << motMystere << "\n";
 
-   /* do
+    do
     {
         cout << "Quel est ce mot?" << endl;
         cin >> motMystere;
@@ -35,12 +37,11 @@ int main()
     } while (motMystere != "Procra");
    
         cout << "Bravo!" << endl;
-        return 0;*/
+        return 0;
 
     //2 : On mélange les lettres du mot
         
         srand(time(0));
-        
         int position = rand() % motMystere.size();
         motMystere.erase(4, 1);
         cout << "Lettre au hasard :" << motMystere[4,1];
