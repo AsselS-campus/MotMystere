e16d617e612e6c19a72290ba5e1574f046c26934
AsselS-campus <assel.sultangazieva@le-campus-numerique.fr>
2021-04-23 14:56:13 +0200

    getUserInput

--- MotMystere.cpp
+++ MotMystere.cpp
@@ -5,48 +5,25 @@
 #include <string>
 #include <ctime>
 #include <cstdlib>
-
 using namespace std;
+
+string getUserInput() {
+    string motMystere;
+    cout << "Entrer un mot : ";
+    cin >> motMystere;
+    cout << "Le mot est :" << motMystere;
+    return motMystere;
+}
 
 int main()
 {
-   
-    
-    //1 : On demande de saisir un mot
-
-    string motMystere("Procra");
-    cout << "Entrer un mot : ";
-    std::cin >> motMystere;
-    cout << "Le mot est :" << motMystere;
+    string motMystere;
+    motMystere=getUserInput();
     return 0;
-
-    //3 : On demande à l'utilisateur quel est le mot mystère
-  
-    cout << "Saisissez un mot : " << endl;;
-    cin >> motMystere;
-    cout << "Le mot est :" << motMystere << "\n";
-
-    do
-    {
-        cout << "Quel est ce mot?" << endl;
-        cin >> motMystere;
-        cout << "Ce n'est pas le mot?" << endl;
-        cout << "Quel est ce mot?" << endl;
-        cin >> motMystere;
-        
-    } while (motMystere != "Procra");
    
-        cout << "Bravo!" << endl;
-        return 0;
-
-    //2 : On mélange les lettres du mot
-        
-        srand(time(0));
-        int position = rand() % motMystere.size();
-        motMystere.erase(4, 1);
-        cout << "Lettre au hasard :" << motMystere[4,1];
-       
-        return 0;
+  
+    
+    
 
 }
 
