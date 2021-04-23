82ba7338a7b3757c143245a6f535e9e3a0d9bf71
asselSultangazieva <73889877+AsselS-campus@users.noreply.github.com>
2021-04-23 10:55:48 +0200

    Pr test (#3)
    * etapes 2,3

--- MotMystere.cpp
+++ MotMystere.cpp
@@ -2,19 +2,52 @@
 //
 
 #include <iostream>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+   
+    /*string motMystere("Procrastination");
+    //1 : On demande de saisir un mot
+    cout << "Entrer un mot : ";
+    std::cin >> motMystere;
+    cout << "Le mot est :" << motMystere;
+    return 0;*/
+
+    //3 : On demande à l'utilisateur quel est le mot mystère
+    string motMystere("Procra");
+    cout << "Saisissez un mot : " << endl;;
+    cin >> motMystere;
+    cout << "Le mot est :" << motMystere << "\n";
+
+   /* do
+    {
+        cout << "Quel est ce mot?" << endl;
+        cin >> motMystere;
+        cout << "Ce n'est pas le mot?" << endl;
+        cout << "Quel est ce mot?" << endl;
+        cin >> motMystere;
+        
+    } while (motMystere != "Procra");
+   
+        cout << "Bravo!" << endl;
+        return 0;*/
+
+    //2 : On mélange les lettres du mot
+        
+        srand(time(0));
+        
+        int position = rand() % motMystere.size();
+        motMystere.erase(4, 1);
+        cout << "Lettre au hasard :" << motMystere[4,1];
+       
+        return 0;
+
 }
 
-// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
-// Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
 
-// Astuces pour bien démarrer : 
-//   1. Utilisez la fenêtre Explorateur de solutions pour ajouter des fichiers et les gérer.
-//   2. Utilisez la fenêtre Team Explorer pour vous connecter au contrôle de code source.
-//   3. Utilisez la fenêtre Sortie pour voir la sortie de la génération et d'autres messages.
-//   4. Utilisez la fenêtre Liste d'erreurs pour voir les erreurs.
-//   5. Accédez à Projet > Ajouter un nouvel élément pour créer des fichiers de code, ou à Projet > Ajouter un élément existant pour ajouter des fichiers de code existants au projet.
-//   6. Pour rouvrir ce projet plus tard, accédez à Fichier > Ouvrir > Projet et sélectionnez le fichier .sln.
+
